ahm-forks/xfce4-windowck-plugin

commit 3f82c6deefa5782f6fb5bcef4c76f7f09326b260

Exit early and remove plugin on non X11 environments
Until #37 is addressed.

common/wck-plugin.c
@@ -24,6 +24,9 @@
 
 #include <string.h>
 #include <gtk/gtk.h>
+#ifdef GDK_WINDOWING_X11
+#include <gdk/gdkx.h>
+#endif
 #include <libxfce4ui/libxfce4ui.h>
 
 #include "wck-plugin.h"
@@ -260,4 +263,27 @@ wck_configure_response (XfcePanelPlugin *plugin, GtkWidget *dialog, gint respons
         /* destroy the properties dialog */
         gtk_widget_destroy (dialog);
     }
+}
+
+
+gboolean
+wck_abort_non_x11_windowing (XfcePanelPlugin *plugin)
+{
+#ifdef GDK_WINDOWING_X11
+    gboolean x11_windowing = GDK_IS_X11_DISPLAY (gdk_display_get_default ());
+#else
+    gboolean x11_windowing = FALSE;
+#endif
+    if (!x11_windowing)
+    {
+        GtkWidget *dialog = xfce_message_dialog_new (NULL, xfce_panel_plugin_get_display_name (plugin), "dialog-error",
+                                                     _("Unsupported windowing environment"), NULL,
+                                                     _("_OK"), GTK_RESPONSE_OK, NULL);
+        gtk_dialog_run (GTK_DIALOG (dialog));
+        gtk_widget_destroy (dialog);
+        xfce_panel_plugin_remove (plugin);
+        return TRUE;
+    }
+
+    return FALSE;
 }

common/wck-plugin.h
@@ -55,6 +55,7 @@ GtkWidget *show_refresh_item (XfcePanelPlugin *plugin);
 GtkWidget *wck_dialog_get_widget (GtkBuilder *builder, const gchar *name);
 void wck_configure_dialog (XfcePanelPlugin *plugin, const gchar *icon_name, GtkWidget *ca, GCallback response_cb, gpointer data);
 void wck_configure_response (XfcePanelPlugin *plugin, GtkWidget *dialog, gint response, WckSettingsCb save_settings, gpointer data);
+gboolean wck_abort_non_x11_windowing (XfcePanelPlugin *plugin);
 
 G_END_DECLS
 

panel-plugin/buttons/wckbuttons.c
@@ -476,6 +476,10 @@ wckbuttons_construct (XfcePanelPlugin *plugin)
     /* setup transation domain */
     xfce_textdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
 
+    /* abort on non X11 environments */
+    if (wck_abort_non_x11_windowing (plugin))
+        return;
+
     /* create the plugin */
     wbp = wckbuttons_new (plugin);
 

panel-plugin/menu/wckmenu.c
@@ -258,6 +258,10 @@ static void wckmenu_construct(XfcePanelPlugin *plugin)
     /* setup transation domain */
     xfce_textdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
 
+    /* abort on non X11 environments */
+    if (wck_abort_non_x11_windowing (plugin))
+        return;
+
     /* create the plugin */
     wmp = wckmenu_new(plugin);
 

panel-plugin/title/wcktitle.c
@@ -254,6 +254,10 @@ wcktitle_construct (XfcePanelPlugin *plugin)
     /* setup transation domain */
     xfce_textdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
 
+    /* abort on non X11 environments */
+    if (wck_abort_non_x11_windowing (plugin))
+        return;
+
     /* create the plugin */
     wtp = wcktitle_new (plugin);
 

commit 9e5a1b5aace8ccd26121879c5b3d32cd2781bed4

Fix removal of plugin at startup on wayland
It caused a crash when plugin is run as internal. It's still a bit buggy
with XFCE_PANEL_PLUGIN_REGISTER_WITH_CHECK, but it's better (harmless
critical warning, no crash).

Fixes: 3f82c6deefa5782f6fb5bcef4c76f7f09326b260

common/wck-plugin.c
@@ -271,7 +271,7 @@ wck_configure_response (XfcePanelPlugin *plugin, GtkWidget *dialog, gint respons
 
 
 gboolean
-wck_abort_non_x11_windowing (XfcePanelPlugin *plugin)
+wck_check_x11_windowing (GdkScreen *screen)
 {
 #ifdef GDK_WINDOWING_X11
     gboolean x11_windowing = GDK_IS_X11_DISPLAY (gdk_display_get_default ());
@@ -280,14 +280,13 @@ wck_abort_non_x11_windowing (XfcePanelPlugin *plugin)
 #endif
     if (!x11_windowing)
     {
-        GtkWidget *dialog = xfce_message_dialog_new (NULL, xfce_panel_plugin_get_display_name (plugin), "dialog-error",
+        GtkWidget *dialog = xfce_message_dialog_new (NULL, NULL, "dialog-error",
                                                      _("Unsupported windowing environment"), NULL,
                                                      _("_OK"), GTK_RESPONSE_OK, NULL);
         gtk_dialog_run (GTK_DIALOG (dialog));
         gtk_widget_destroy (dialog);
-        xfce_panel_plugin_remove (plugin);
-        return TRUE;
+        return FALSE;
     }
 
-    return FALSE;
+    return TRUE;
 }

common/wck-plugin.h
@@ -55,7 +55,7 @@ GtkWidget *show_refresh_item (XfcePanelPlugin *plugin);
 GtkWidget *wck_dialog_get_widget (GtkBuilder *builder, const gchar *name);
 void wck_configure_dialog (XfcePanelPlugin *plugin, const gchar *icon_name, GtkWidget *ca, GCallback response_cb, gpointer data);
 void wck_configure_response (XfcePanelPlugin *plugin, GtkWidget *dialog, gint response, WckSettingsCb save_settings, gpointer data);
-gboolean wck_abort_non_x11_windowing (XfcePanelPlugin *plugin);
+gboolean wck_check_x11_windowing (GdkScreen *screen);
 
 G_END_DECLS
 

panel-plugin/buttons/wckbuttons.c
@@ -472,10 +472,6 @@ wckbuttons_construct (XfcePanelPlugin *plugin)
     /* setup transation domain */
     xfce_textdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
 
-    /* abort on non X11 environments */
-    if (wck_abort_non_x11_windowing (plugin))
-        return;
-
     /* create the plugin */
     wbp = wckbuttons_new (plugin);
 
@@ -529,4 +525,4 @@ wckbuttons_construct (XfcePanelPlugin *plugin)
 
 
 /* register the plugin */
-XFCE_PANEL_PLUGIN_REGISTER (wckbuttons_construct);
+XFCE_PANEL_PLUGIN_REGISTER_WITH_CHECK (wckbuttons_construct, wck_check_x11_windowing);

panel-plugin/menu/wckmenu.c
@@ -254,10 +254,6 @@ static void wckmenu_construct(XfcePanelPlugin *plugin)
     /* setup transation domain */
     xfce_textdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
 
-    /* abort on non X11 environments */
-    if (wck_abort_non_x11_windowing (plugin))
-        return;
-
     /* create the plugin */
     wmp = wckmenu_new(plugin);
 
@@ -303,4 +299,4 @@ static void wckmenu_construct(XfcePanelPlugin *plugin)
 
 
 /* register the plugin */
-XFCE_PANEL_PLUGIN_REGISTER(wckmenu_construct);
+XFCE_PANEL_PLUGIN_REGISTER_WITH_CHECK(wckmenu_construct, wck_check_x11_windowing);

panel-plugin/title/wcktitle.c
@@ -251,10 +251,6 @@ wcktitle_construct (XfcePanelPlugin *plugin)
     /* setup transation domain */
     xfce_textdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
 
-    /* abort on non X11 environments */
-    if (wck_abort_non_x11_windowing (plugin))
-        return;
-
     /* create the plugin */
     wtp = wcktitle_new (plugin);
 
@@ -312,4 +308,4 @@ wcktitle_construct (XfcePanelPlugin *plugin)
 
 
 /* register the plugin */
-XFCE_PANEL_PLUGIN_REGISTER (wcktitle_construct);
+XFCE_PANEL_PLUGIN_REGISTER_WITH_CHECK (wcktitle_construct, wck_check_x11_windowing);